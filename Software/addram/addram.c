f6ceef375799208269b1a3c8475cce6e5577d707
Matt Harlum <955874+LIV2@users.noreply.github.com>
2023-02-23 11:01:54 +0000

    Add license to software

--- Software/addram/addram.c
+++ Software/addram/addram.c
@@ -15,7 +15,6 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-
 #include <exec/execbase.h>
 #include <proto/exec.h>
 #include <proto/expansion.h>
